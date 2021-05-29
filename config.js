d774811549442b2ee896421aa9a9bcdf1c1ef285
ValliappanValliappan <gtvalliappan@gmail.com>
2021-05-29 03:11:41 -0700

    removed private info

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
-    consumer_key:         'Yrsk4UNGeBbKViodlIDfnuR97',
-    consumer_secret:      'Xs7hHXv1G7IKxAi4DnessU3vwoaWLVu1AndalMAt4SmGZDOhKu',
-    access_token:         '1397837646701817858-JmKhny3X5s8uH60NPrLtK5hYzQ9Fqq',
-    access_token_secret:  'K834Yiq2YnmQLgKJ3IGT5cTAjPOPyaV0ICKRXttfqMegD',
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...'
   }
